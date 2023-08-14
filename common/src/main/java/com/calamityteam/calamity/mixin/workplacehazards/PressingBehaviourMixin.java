ea8ba132e2698ec53aab57ca7e1c58c068c621b6
Sascha T <info@sascha-t.de>
2023-08-14 23:50:30 +0200

    feat:Make press damage depend on stress/speed
    See PressingBehaviourMixin.java
    2048 free stress units = 100% of HP
    256  RPM               = 10%  of helmet durability

--- common/src/main/java/com/calamityteam/calamity/mixin/workplacehazards/PressingBehaviourMixin.java
+++ common/src/main/java/com/calamityteam/calamity/mixin/workplacehazards/PressingBehaviourMixin.java
@@ -1,16 +1,20 @@
-package com.calamityteam.calamity.mixin;
+package com.calamityteam.calamity.mixin.workplacehazards;
 
+import com.calamityteam.calamity.mixin.KineticBlockEntityAccessor;
 import com.simibubi.create.content.kinetics.belt.behaviour.BeltProcessingBehaviour;
 import com.simibubi.create.content.kinetics.press.BeltPressingCallbacks;
+import com.simibubi.create.content.kinetics.press.MechanicalPressBlockEntity;
 import com.simibubi.create.content.kinetics.press.PressingBehaviour;
 
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 import net.minecraft.world.level.block.TrapDoorBlock;
@@ -51,16 +55,21 @@ public abstract class PressingBehaviourMixin extends BeltProcessingBehaviour {
 	@Shadow
 	public boolean running;
 
-	/*
-				0         = Start
-				CYCLE / 2 = Process
-				CYCLE     = End
+    @Shadow public PressingBehaviour.PressingBehaviourSpecifics specifics;
 
-				Force players into a crouched position from 20% to 80%
-				Damage them when within 40% to 60% or kill on proc
-			 */
+    /*
+                    0         = Start
+                    CYCLE / 2 = Process
+                    CYCLE     = End
+
+                    Force players into a crouched position from 20% to 80%
+                    Damage them when within 40% to 60% or kill on proc
+                 */
 	@Inject(at = @At("HEAD"), method = "tick", remap = false)
 	void tick(CallbackInfo ci) {
+        if(getWorld().isClientSide)
+            return;
+
 		Level world = getWorld();
 		BlockPos pos = getPos();
 
@@ -70,8 +79,20 @@ public abstract class PressingBehaviourMixin extends BeltProcessingBehaviour {
 		if (!targets.isEmpty() && !running) // if there are people to crush, and it isn't running yet
 			start(PressingBehaviour.Mode.WORLD); // then maybe you should start running :)
 
+        MechanicalPressBlockEntity mechanicalPress = (MechanicalPressBlockEntity) blockEntity;
+        KineticBlockEntityAccessor accessor = (KineticBlockEntityAccessor) mechanicalPress;
+
+        float stressCoefficient = Math.min(accessor.getMaxStress() - accessor.getStress(), 2048) / 2048; // 2048 is instant kill (@todo: config)
+        float speedCoefficient  = (specifics.getKineticSpeed() / 256f) * 0.1f; // 256 is 10% @todo: double config
+
 		if (cyclePosition == 0.5) // if the cycle is at it's half-point, meaning we have reached the ground
-			for (LivingEntity target : targets)
-				target.hurt(DAMAGE_SOURCE_CRUSHING, target.getHealth()); // we hurt everyone
+			for (LivingEntity target : targets) {
+                if(target instanceof Player p && p.isCreative())
+                    continue; // do not harm creative players' items
+                ItemStack headSlot = target.getItemBySlot(EquipmentSlot.HEAD);
+                headSlot.hurt((int) (headSlot.getMaxDamage() * speedCoefficient), world.getRandom(), null);
+
+                target.hurt(DAMAGE_SOURCE_CRUSHING, target.getHealth() * stressCoefficient); // we hurt everyone
+            }
 	}
 }
