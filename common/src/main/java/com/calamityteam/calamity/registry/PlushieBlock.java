3b5edaec3e6b5091fd29bca7600ddd002b15f1ac
PriestOfFerns <timofeydenin@gmail.com>
2023-08-16 14:48:39 +0300

    fix: made plushieBlock actually work

--- common/src/main/java/com/calamityteam/calamity/registry/PlushieBlock.java
+++ common/src/main/java/com/calamityteam/calamity/registry/PlushieBlock.java
@@ -1,17 +1,31 @@
 package com.calamityteam.calamity.registry;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
-public class PlushieBlock extends Block {
-	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+import javax.annotation.Nullable;
+
+public class PlushieBlock extends HorizontalDirectionalBlock {
+
 
 	public PlushieBlock(Properties properties) {
 		super(properties);
 		this.registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+		return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+	}
+
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
 	}
 }
