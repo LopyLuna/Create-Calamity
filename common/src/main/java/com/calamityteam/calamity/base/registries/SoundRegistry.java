7cfc0c6b66232abfe7d54647dd62876f9b285f71
JR1811 <janreichelt77@gmail.com>
2023-08-16 04:44:15 +0200

    fix: made SoundEvents accessible

--- common/src/main/java/com/calamityteam/calamity/base/registries/SoundRegistry.java
+++ common/src/main/java/com/calamityteam/calamity/base/registries/SoundRegistry.java
@@ -7,20 +7,17 @@ import com.calamityteam.calamity.CreateCalamity;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 
 public class SoundRegistry {
-	static {
-		final RegistryEntry<SoundEvent> PLUSHIE_506 = registerSound("plushie_506");
-		final RegistryEntry<SoundEvent> PLUSHIE_ILLUC = registerSound("plushie_illuc");
-		final RegistryEntry<SoundEvent> PLUSHIE_KELAD8 = registerSound("plushie_kelad8");
-		final RegistryEntry<SoundEvent> PLUSHIE_MILKYFUR_BLUSH = registerSound("plushie_milkyfur_blush");
-		final RegistryEntry<SoundEvent> PLUSHIE_MILKYFUR_HONK = registerSound("plushie_milkyfur_honk");
-		final RegistryEntry<SoundEvent> PLUSHIE_OUTCRAFT = registerSound("plushie_outcraft");
-		final RegistryEntry<SoundEvent> PLUSHIE_REDS = registerSound("plushie_reds");
-		final RegistryEntry<SoundEvent> PLUSHIE_SASCHA = registerSound("plushie_sascha");
-		final RegistryEntry<SoundEvent> PLUSHIE_SHIROJR = registerSound("plushie_shirojr");
-		final RegistryEntry<SoundEvent> PLUSHIE_THESPOKESMAN = registerSound("plushie_tespokesman");
-		final RegistryEntry<SoundEvent> PLUSHIE_TO0PA = registerSound("plushie_to0pa");
-
-	}
+	public static final RegistryEntry<SoundEvent> PLUSHIE_506 = registerSound("plushie_506");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_ILLUC = registerSound("plushie_illuc");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_KELAD8 = registerSound("plushie_kelad8");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_MILKYFUR_BLUSH = registerSound("plushie_milkyfur_blush");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_MILKYFUR_HONK = registerSound("plushie_milkyfur_honk");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_OUTCRAFT = registerSound("plushie_outcraft");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_REDS = registerSound("plushie_reds");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_SASCHA = registerSound("plushie_sascha");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_SHIROJR = registerSound("plushie_shirojr");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_THESPOKESMAN = registerSound("plushie_tespokesman");
+	public static final RegistryEntry<SoundEvent> PLUSHIE_TO0PA = registerSound("plushie_to0pa");
 
 	/**
 	 * Register sound events <br><br>
