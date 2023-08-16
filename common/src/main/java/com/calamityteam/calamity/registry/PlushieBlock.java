a50940b57f02f9b7691f150113027fae1ddcb039
JR1811 <janreichelt77@gmail.com>
2023-08-16 15:52:10 +0200

    feat: added sounds to PlushieBlock interaction

--- common/src/main/java/com/calamityteam/calamity/registry/PlushieBlock.java
+++ common/src/main/java/com/calamityteam/calamity/registry/PlushieBlock.java
@@ -1,21 +1,31 @@
 package com.calamityteam.calamity.registry;
 
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-
-import javax.annotation.Nullable;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class PlushieBlock extends HorizontalDirectionalBlock {
+	private final @Nullable List<SoundEvent> sounds;
 
-
-	public PlushieBlock(Properties properties) {
+	public PlushieBlock(Properties properties, @Nullable List<SoundEvent> sounds) {
 		super(properties);
 		this.registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
+		this.sounds = sounds;
 	}
 
 	@Override
@@ -27,5 +37,17 @@ public class PlushieBlock extends HorizontalDirectionalBlock {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		builder.add(FACING);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+		if (level.isClientSide()) return super.use(state, level, pos, player, hand, hit);
+		if (this.sounds == null || sounds.isEmpty()) return InteractionResult.SUCCESS;
+
+		int random = level.getRandom().nextInt(sounds.size() - 1);
+		level.playSound(null, pos, this.sounds.get(random), SoundSource.BLOCKS, 2f, 1f);
+
+		return InteractionResult.SUCCESS;
 	}
 }
--- common/src/main/java/com/calamityteam/calamity/registry/PlushieRegistry.java
+++ common/src/main/java/com/calamityteam/calamity/registry/PlushieRegistry.java
@@ -1,13 +1,14 @@
 package com.calamityteam.calamity.registry;
 
-import com.calamityteam.calamity.Calamity;
-import com.calamityteam.calamity.registry.CLCreativeModeTab;
-import com.tterrag.registrate.Registrate;
-import com.tterrag.registrate.util.entry.BlockEntry;
+import java.util.List;
 
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+
+import com.calamityteam.calamity.Calamity;
+import com.tterrag.registrate.util.entry.BlockEntry;
 
 
 public class PlushieRegistry {
@@ -15,35 +16,55 @@ public class PlushieRegistry {
 		Calamity.REGISTRATE.creativeModeTab(() -> CLCreativeModeTab.CALAMITY_PLUSHIE_TAB);
 	}
 
-		public static final BlockEntry<PlushieBlock> HEROBRINE = Calamity.REGISTRATE.block("herobrine", PlushieBlock::new)
-			.properties(BlockBehaviour.Properties::noOcclusion)
-			.addLayer(() -> RenderType::cutoutMipped)
-			.item(BlockItem::new).build().register(); // this one doesnt match on purpose, pls dont fix
-		public static final BlockEntry<PlushieBlock> TO0PA_PLUSH = newPlush("to0pa");
-		public static final BlockEntry<PlushieBlock> SASCHA_PLUSH = newPlush("sascha");
-		public static final BlockEntry<PlushieBlock> MILKYFUR_PLUSH = newPlush("milkyfur");
-		public static final BlockEntry<PlushieBlock> PLUSH_506 = newPlush("506");
-		public static final BlockEntry<PlushieBlock> OUTCRAFT_PLUSH = newPlush("outcraft");
-		public static final BlockEntry<PlushieBlock> ILLUC_PLUSH = newPlush("illuc");
-		public static final BlockEntry<PlushieBlock> SPYDNEL_PLUSH = newPlush("spydnel");
-		public static final BlockEntry<PlushieBlock> RAEEEEE_PLUSH = newPlush("raeeeee");
-		public static final BlockEntry<PlushieBlock> SHIRO_JR_PLUSH = newPlush("shiro_jr");
-		public static final BlockEntry<PlushieBlock> REDS_PLUSH = newPlush("reds");
-		public static final BlockEntry<PlushieBlock> TOMATO_SOUPTER_PLUSH = newPlush("tomato_soupter");
-		public static final BlockEntry<PlushieBlock> POTATO_PLUSH = newPlush("potato");
-		public static final BlockEntry<PlushieBlock> NULL_PLUSH = newPlush("null");
-		public static final BlockEntry<PlushieBlock> BAAB_PLUSH = newPlush("baab");
-		public static final BlockEntry<PlushieBlock> ITHUNDXR_PLUSH = newPlush("ithundxr");
-		public static final BlockEntry<PlushieBlock> FERN_PLUSH = newPlush("fern");
-		public static final BlockEntry<PlushieBlock> CAKE_PLUSH = newPlush("cake");
+	public static final BlockEntry<PlushieBlock> HEROBRINE = Calamity.REGISTRATE.block("herobrine",
+			properties -> new PlushieBlock(properties, null))
+		.properties(BlockBehaviour.Properties::noOcclusion)
+		.addLayer(() -> RenderType::cutoutMipped)
+		.item(BlockItem::new).build().register(); // this one doesnt match on purpose, pls dont fix
+	public static final BlockEntry<PlushieBlock> TO0PA_PLUSH = newPlush("to0pa",
+		List.of(CLSounds.PLUSHIE_TO0PA));
+	public static final BlockEntry<PlushieBlock> SASCHA_PLUSH = newPlush("sascha",
+		List.of(CLSounds.PLUSHIE_SASCHA));
+	public static final BlockEntry<PlushieBlock> MILKYFUR_PLUSH = newPlush("milkyfur",
+		List.of(CLSounds.PLUSHIE_MILKYFUR_HONK, CLSounds.PLUSHIE_MILKYFUR_BLUSH));
+	public static final BlockEntry<PlushieBlock> PLUSH_506 = newPlush("506",
+		List.of(CLSounds.PLUSHIE_506));
+	public static final BlockEntry<PlushieBlock> OUTCRAFT_PLUSH = newPlush("outcraft",
+		List.of(CLSounds.PLUSHIE_OUTCRAFT));
+	public static final BlockEntry<PlushieBlock> ILLUC_PLUSH = newPlush("illuc",
+		List.of(CLSounds.PLUSHIE_ILLUC));
+	public static final BlockEntry<PlushieBlock> SPYDNEL_PLUSH = newPlush("spydnel",
+		List.of());
+	public static final BlockEntry<PlushieBlock> RAEEEEE_PLUSH = newPlush("raeeeee",
+		List.of());
+	public static final BlockEntry<PlushieBlock> SHIROJR_PLUSH = newPlush("shirojr",
+		List.of(CLSounds.PLUSHIE_SHIROJR));
+	public static final BlockEntry<PlushieBlock> REDS_PLUSH = newPlush("reds",
+		List.of(CLSounds.PLUSHIE_REDS));
+	public static final BlockEntry<PlushieBlock> TOMATO_SOUPTER_PLUSH = newPlush("tomato_soupter",
+		List.of());
+	public static final BlockEntry<PlushieBlock> POTATO_PLUSH = newPlush("potato",
+		List.of());
+	public static final BlockEntry<PlushieBlock> NULL_PLUSH = newPlush("null",
+		List.of());
+	public static final BlockEntry<PlushieBlock> BAAB_PLUSH = newPlush("baab",
+		List.of());
+	public static final BlockEntry<PlushieBlock> ITHUNDXR_PLUSH = newPlush("ithundxr",
+		List.of());
+	public static final BlockEntry<PlushieBlock> FERN_PLUSH = newPlush("fern",
+		List.of());
+	public static final BlockEntry<PlushieBlock> CAKE_PLUSH = newPlush("cake",
+		List.of());
 
-	public static BlockEntry<PlushieBlock> newPlush(String name) {
-		return Calamity.REGISTRATE.block("plushie_" + name, PlushieBlock::new)
+	public static BlockEntry<PlushieBlock> newPlush(String name, List<SoundEvent> sounds) {
+		return Calamity.REGISTRATE.block("plushie_" + name, properties -> new PlushieBlock(properties, sounds))
 			.properties(BlockBehaviour.Properties::noOcclusion)
 			.addLayer(() -> RenderType::cutoutMipped)
 			.item(BlockItem::new)
 			.build()
 			.register();
 	}
-	public static void register() {}
+
+	public static void register() {
+	}
 }
