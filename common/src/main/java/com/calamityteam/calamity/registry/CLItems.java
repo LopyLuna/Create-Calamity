572929e770f846fbf49a128b535824c8751dd17b
rae <raej0000@gmail.com>
2023-08-17 13:29:28 -0700

    error: thigh highs render wrongly

--- common/src/main/java/com/calamityteam/calamity/registry/CLItems.java
+++ common/src/main/java/com/calamityteam/calamity/registry/CLItems.java
@@ -3,22 +3,20 @@ package com.calamityteam.calamity.registry;
 import static com.calamityteam.calamity.Calamity.REGISTRATE;
 
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.DyeableArmorItem;
-import net.minecraft.world.item.Item;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
 
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 
 public class CLItems {
 	static {
 		REGISTRATE.creativeModeTab(() -> CLCreativeModeTab.CALAMITY_TAB);
 	}
 
-	public static final ItemEntry<? extends DyeableArmorItem> THIGH_HIGHS = REGISTRATE.item("thigh_highs",
-		p -> new DyeableArmorItem(CLArmorMaterials.THIGH_HIGH, EquipmentSlot.LEGS, p))
+	public static final ItemEntry<? extends CLDyeableArmorItem> THIGH_HIGHS = REGISTRATE.item("thigh_highs",
+		p -> new CLDyeableArmorItem(CLArmorMaterials.THIGH_HIGH, EquipmentSlot.LEGS, p))
 		.register();
 
 	public static void register() {}
