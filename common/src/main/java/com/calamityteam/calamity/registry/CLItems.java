3d1535a9831cb2b0852bb23f525877cf9511cadf
rae <raej0000@gmail.com>
2023-08-17 16:35:28 -0700

    chore: lang entry and cleanup, fixme CLThighHighItem

--- common/src/main/java/com/calamityteam/calamity/registry/CLItems.java
+++ common/src/main/java/com/calamityteam/calamity/registry/CLItems.java
@@ -3,12 +3,9 @@ package com.calamityteam.calamity.registry;
 import static com.calamityteam.calamity.Calamity.REGISTRATE;
 
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.DyeableArmorItem;
+import net.minecraft.world.item.Rarity;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
-
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
 
 public class CLItems {
 	static {
@@ -17,7 +14,11 @@ public class CLItems {
 
 	public static final ItemEntry<? extends CLThighHighItem> THIGH_HIGHS = REGISTRATE.item("thigh_highs",
 		p -> new CLThighHighItem(CLArmorMaterials.THIGH_HIGH, EquipmentSlot.LEGS, p))
+		.lang("Programmer Thigh Highs")
+		.properties(p -> p.rarity(Rarity.EPIC))
 		.register();
 
-	public static void register() {}
+	public static void register() {
+		CLThighHighItem.register();
+	}
 }
--- common/src/main/java/com/calamityteam/calamity/registry/CLThighHighItem.java
+++ common/src/main/java/com/calamityteam/calamity/registry/CLThighHighItem.java
@@ -1,5 +1,7 @@
 package com.calamityteam.calamity.registry;
 
+import com.calamityteam.calamity.Calamity;
+import io.github.fabricators_of_create.porting_lib.item.CustomEnchantmentsItem;
 import net.fabricmc.fabric.api.event.AutoInvokingEvent;
 
 import net.minecraft.nbt.CompoundTag;
@@ -11,24 +13,36 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.DyeableArmorItem;
-import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 
-public class CLThighHighItem extends DyeableArmorItem {
+import java.util.Map;
+
+public class CLThighHighItem extends DyeableArmorItem implements CustomEnchantmentsItem {
 	public CLThighHighItem(ArmorMaterial material, EquipmentSlot slot, Properties properties) {
 		super(material, slot, properties);
+		this.getDefaultInstance().enchant(Enchantments.BINDING_CURSE,1);
+		// fixme enchant not applied
 	}
+
+	/*@Override
+	public ItemStack getDefaultInstance() {
+		ItemStack out = new ItemStack(this);
+		out.enchant(Enchantments.BINDING_CURSE,1);
+		Calamity.LOGGER.debug("apply curse" + out);
+		return out;
+	}*/
 
 	@Override
 	public int getColor(ItemStack stack) {
 		CompoundTag compoundTag = stack.getTagElement("display");
 		return compoundTag != null && compoundTag.contains("color", 99) ? compoundTag.getInt("color") : 16099768;
 	}
-
 	public static boolean isWornBy(Entity entity) {
 		return !getWornItem(entity).isEmpty();
 	}
-
 	public static ItemStack getWornItem(Entity entity) {
 		if (!(entity instanceof LivingEntity livingEntity)) {
 			return ItemStack.EMPTY;
@@ -45,4 +59,19 @@ public class CLThighHighItem extends DyeableArmorItem {
 			entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2, 0, true, false, true));
 		}
 	}
+
+	/**
+	 * modify the enchantments found on the given stack.
+	 * The map is a map of enchantments to the level found on the item. This map is mutable and may be directly modified.
+	 *
+	 * @param enchantments
+	 * @param stack
+	 */
+	@Override
+	public void modifyEnchantments(Map<Enchantment, Integer> enchantments, ItemStack stack) {
+		enchantments.put(Enchantments.BINDING_CURSE,1);
+		Calamity.LOGGER.debug("add enchant to " + enchantments);
+	}
+
+	public static void register() {}
 }
