5dbea76dacadf4643e219ade5c3363bff8315d28
JR1811 <janreichelt77@gmail.com>
2023-08-19 11:11:19 +0200

    feat: added SculkSensor feature to MaidArmorItem

---
+++ common/src/main/java/com/calamityteam/calamity/mixin/SculkSensorBlockMixin.java
@@ -0,0 +1,35 @@
+package com.calamityteam.calamity.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.SculkSensorBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.gameevent.GameEventListener;
+
+import com.calamityteam.calamity.base.item.thigh_high.MaidArmorItem;
+
+@Mixin(SculkSensorBlockEntity.class)
+public abstract class SculkSensorBlockMixin extends BlockEntity {
+
+
+	public SculkSensorBlockMixin(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
+		super(type, pos, blockState);
+	}
+
+	@Inject(method = "onSignalReceive", at = @At(value = "HEAD"), cancellable = true)
+	private void calamity$blockSculkSensorWithItem(ServerLevel level, GameEventListener listener, BlockPos sourcePos, GameEvent gameEvent, Entity sourceEntity, Entity projectileOwner, float distance, CallbackInfo ci) {
+		if (!(sourceEntity instanceof Player player)) return;
+		if (!(player.getInventory().getArmor(0).getItem() instanceof MaidArmorItem)) return;
+		ci.cancel();
+	}
+}
